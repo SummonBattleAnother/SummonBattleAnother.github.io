d7b1a20250c536d9084ace7655ff9ee4c07d7d60
배준혁 <baejunehyuck@hd.com>
2024-09-03 10:01:05 +0900

    change

--- assets/js/load-heroes.js
+++ assets/js/load-heroes.js
@@ -2,7 +2,7 @@
 let heroes = [];
 let filteredHeroes = [];
 let selectedRole = 'all';
-let validHeroNo = 51; // 해당번호의 영웅까지만 작성이 되었습니다
+let validHeroNo = 52; // 해당번호의 영웅까지만 작성이 되었습니다
 
 const colors = {
   '일반': 'rgb(0, 0, 0)',
